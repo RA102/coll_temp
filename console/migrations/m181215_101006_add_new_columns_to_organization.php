1c2a42bbbc91359ea504ad5745d036d1fd08600c
Azamat Sarsenbayev <sarsenbayev.a.s@gmail.com>
2018-12-15 16:58:45 +0600

    fix(migrations): add missing columns to institution

---
+++ console/migrations/m181215_101006_add_new_columns_to_organization.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181215_101006_add_new_columns_to_organization
+ */
+class m181215_101006_add_new_columns_to_organization extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('organization.institution', 'max_shift', $this->smallInteger()->defaultValue(0));
+        $this->addColumn('organization.institution', 'min_grade', $this->smallInteger()->defaultValue(0));
+        $this->addColumn('organization.institution', 'enable_fraction', $this->boolean()->defaultValue(false));
+        $this->addColumn('organization.institution', 'is_test', $this->boolean()->defaultValue(false));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('organization.institution', 'is_test');
+        $this->dropColumn('organization.institution', 'enable_fraction');
+        $this->dropColumn('organization.institution', 'min_grade');
+        $this->dropColumn('organization.institution', 'max_shift');
+    }
+}
