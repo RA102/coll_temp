b6decb9734d900cb4d2d3a784f1e1465bdb1834f
Azamat Sarsenbayev <sarsenbayev.a.s@gmail.com>
2018-12-26 22:30:29 +0600

    fix(signup): change service location

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -3,7 +3,7 @@
 namespace frontend\controllers;
 
 use common\forms\auth\LoginForm;
-use common\services\auth\LoginService;
+use common\services\pds\LoginService;
 use common\services\organization\InstitutionApplicationService;
 use Yii;
 use yii\base\InvalidParamException;
