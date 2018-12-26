648c0c9f64806791241c90c259859628abf77883
Azamat Sarsenbayev <sarsenbayev.a.s@gmail.com>
2018-12-26 12:40:03 +0600

    feat(signup): add migration for educational form

---
+++ console/migrations/m181226_050639_create_educational_form_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `educational_form`.
+ */
+class m181226_050639_create_educational_form_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('organization.educational_form', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'caption' => 'jsonb',
+            'status' => $this->smallInteger()->defaultValue(1),
+            'is_deleted' => $this->boolean(),
+            'create_ts' => $this->dateTime()->notNull()->defaultValue('now()'),
+            'oid' => $this->bigInteger(),
+            'update_ts' => $this->dateTime()->notNull()->defaultValue('now()'),
+            'delete_ts' => $this->dateTime()->null(),
+        ]);
+
+        $this->db->pdo->exec(file_get_contents(Yii::getAlias("@console/resources/db_dumps/educational_form.sql")));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('organization.educational_form');
+    }
+}
