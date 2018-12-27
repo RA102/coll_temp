bda9a4771101613cb6fd3565af5d03a749c5eba2
Azamat Sarsenbayev <sarsenbayev.a.s@gmail.com>
2018-12-28 00:03:18 +0600

    feat(PersonInstituionLink): add model and migration

---
+++ common/models/link/PersonInstitutionLink.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace common\models\link;
+
+use common\models\organization\Institution;
+use common\models\person\Person;
+use Yii;
+
+/**
+ * This is the model class for table "link.person_institution_link".
+ *
+ * @property int $id
+ * @property int $person_id
+ * @property int $institution_id
+ * @property string $from_ts
+ * @property string $to_ts
+ * @property int $status
+ * @property string $person_type
+ * @property int $index
+ * @property bool $is_deleted
+ * @property string $create_ts
+ * @property string $position
+ * @property string $document_submission_date
+ * @property string $comment
+ * @property int $condition
+ * @property int $unfasten_reason_id
+ * @property bool $is_pluralist
+ * @property string $import_ts
+ * @property int $document_number
+ */
+class PersonInstitutionLink extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'link.person_institution_link';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['person_id', 'institution_id', 'status', 'index', 'condition', 'unfasten_reason_id', 'document_number'], 'default', 'value' => null],
+            [['person_id', 'institution_id', 'status', 'index', 'condition', 'unfasten_reason_id', 'document_number'], 'integer'],
+            [['from_ts', 'to_ts', 'create_ts', 'document_submission_date', 'import_ts'], 'safe'],
+            [['is_deleted', 'is_pluralist'], 'boolean'],
+            [['person_type', 'position'], 'string', 'max' => 100],
+            [['comment'], 'string', 'max' => 255],
+            [['institution_id'], 'exist', 'skipOnError' => true, 'targetClass' => Institution::className(), 'targetAttribute' => ['institution_id' => 'id']],
+            [['person_id'], 'exist', 'skipOnError' => true, 'targetClass' => Person::className(), 'targetAttribute' => ['person_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'person_id' => Yii::t('app', 'Person ID'),
+            'institution_id' => Yii::t('app', 'Institution ID'),
+            'from_ts' => Yii::t('app', 'From Ts'),
+            'to_ts' => Yii::t('app', 'To Ts'),
+            'status' => Yii::t('app', 'Status'),
+            'person_type' => Yii::t('app', 'Person Type'),
+            'index' => Yii::t('app', 'Index'),
+            'is_deleted' => Yii::t('app', 'Is Deleted'),
+            'create_ts' => Yii::t('app', 'Create Ts'),
+            'position' => Yii::t('app', 'Position'),
+            'document_submission_date' => Yii::t('app', 'Document Submission Date'),
+            'comment' => Yii::t('app', 'Comment'),
+            'condition' => Yii::t('app', 'Condition'),
+            'unfasten_reason_id' => Yii::t('app', 'Unfasten Reason ID'),
+            'is_pluralist' => Yii::t('app', 'Is Pluralist'),
+            'import_ts' => Yii::t('app', 'Import Ts'),
+            'document_number' => Yii::t('app', 'Document Number'),
+        ];
+    }
+}
---
+++ console/migrations/m181227_171714_create_person_institution_link_table.php
@@ -0,0 +1,69 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `person_institution_link`.
+ */
+class m181227_171714_create_person_institution_link_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->execute("
+            CREATE SCHEMA link;
+        ");
+
+        $this->createTable('link.person_institution_link', [
+            'id' => $this->primaryKey(),
+            'person_id' => $this->bigInteger(),
+            'institution_id' => $this->bigInteger(),
+            'from_ts' => $this->dateTime()->null(),
+            'to_ts' => $this->dateTime()->null(),
+            'status' => $this->smallInteger()->defaultValue(1),
+            'person_type' => $this->string(100),
+            'index' => $this->bigInteger(),
+            'is_deleted' => $this->boolean(),
+            'create_ts' => $this->dateTime()->notNull()->defaultValue('now()'),
+            'position' => $this->string(100),
+            'document_submission_date' => $this->date(),
+            'comment' => $this->string(255),
+            'condition' => $this->smallInteger(),
+            'unfasten_reason_id' => $this->smallInteger(),
+            'is_pluralist' => $this->boolean(),
+            'import_ts' => $this->dateTime(),
+            'document_number' => $this->bigInteger()
+        ]);
+
+        $this->addForeignKey(
+            'fk_person_institution_link_2_person',
+            'link.person_institution_link',
+            'person_id',
+            'person.person',
+            'id'
+        );
+        $this->addForeignKey(
+            'fk_person_institution_link_2_institution',
+            'link.person_institution_link',
+            'institution_id',
+            'organization.institution',
+            'id'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk_person_institution_link_2_institution', 'link.person_institution_link');
+        $this->dropForeignKey('fk_person_institution_link_2_person', 'link.person_institution_link');
+        $this->dropTable('link.person_institution_link');
+
+        $this->execute("
+            DROP SCHEMA link;
+        ");
+    }
+}
