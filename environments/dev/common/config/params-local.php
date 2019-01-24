3e83523bdf435aea9df9f760c28b8bfcec2cc4ee
Anuar Tastembekov <tastembekov.anuar@gmail.com>
2019-01-24 14:12:53 +0600

    fix(pds): change pds link

--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -1,6 +1,6 @@
 <?php
 return [
     'pds_access_token' => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9hZG1pbi1wZHMuZGV2LnZwbiIsImF1ZCI6Imh0dHA6XC9cL2FkbWluLXBkcy5kZXYudnBuIiwiaWF0IjoxNTE5MTg2MTczLCJuYmYiOjE1MTkxODYxNzMsInVpZCI6MSwiZXhwIjoxNTE5NzkwOTczfQ.cdJCnRXkWPmqBPP4z4M-LUes1Uc9cVjoAUjuuvpr_oQ',
-    'pds_url' => 'http://pds.dev.vpn/',
+    'pds_url' => 'https://pds.citorleu.kz/',
     'session_timeout' => 3600 * 24 * 7
 ];
