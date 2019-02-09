21add245a2c958dba854d537d35f5c85b0a915a5
Azamat Sarsenbayev <sarsenbayev.a.s@gmail.com>
2019-02-09 22:07:02 +0600

    feat(specialities front): add speciality href on left menu

--- frontend/views/layouts/left.php
+++ frontend/views/layouts/left.php
@@ -49,7 +49,7 @@
                         'icon' => 'cog',
                         'url' => '#',
                         'items' => [
-                            ['label' => Yii::t('app','Специальности'), 'icon' => 'file-code-o', 'url' => ['/']],
+                            ['label' => Yii::t('app','Специальности'), 'icon' => 'file-code-o', 'url' => ['/speciality']],
                             ['label' => Yii::t('app','Предметы'), 'icon' => 'file-code-o', 'url' => ['/']],
                             ['label' => Yii::t('app','Учебный процесс'), 'icon' => 'file-code-o', 'url' => ['/']],
                             ['label' => Yii::t('app','Организация'), 'icon' => 'file-code-o', 'url' => ['/']],
