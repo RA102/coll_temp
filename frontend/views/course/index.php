f805a37aea9badd87f3d2a37a2be0181f36b750e
Boolean <erkebulan69@gmail.com>
2019-02-18 15:25:04 +0600

    feat(courses): partial courses crud

---
+++ frontend/views/course/index.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'Courses');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="course-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Course'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'discipline_id',
+            'caption',
+            'grades',
+            'status',
+            //'create_ts',
+            //'update_ts',
+            //'delete_ts',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
