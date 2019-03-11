d9c24499da69b603e9add72aba623d0448431f81
Anuar Tastembekov <tastembekov.anuar@gmail.com>
2019-03-11 17:10:50 +0600

    fix(rbac): Set access control in admin controllers

--- backend/controllers/SpecialityController.php
+++ backend/controllers/SpecialityController.php
@@ -5,6 +5,7 @@ namespace backend\controllers;
 use Yii;
 use common\models\handbook\Speciality;
 use backend\search\handbook\SpecialitySearch;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -20,6 +21,15 @@ class SpecialityController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'allow'   => true,
+                        'roles'   => ['@'],
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
