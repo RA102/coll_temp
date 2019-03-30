c173526592e10890ed885c31b3c17d7b55addec7
Azamat Sarsenbayev <sarsenbayev.a.s@gmail.com>
2019-03-30 11:39:41 +0600

    feat(left menu): hide not implemented module links

--- frontend/views/layouts/left.php
+++ frontend/views/layouts/left.php
@@ -6,17 +6,17 @@
             [
                 'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
                 'items' => [
-                    [
-                        'label' => Yii::t('app','Selection committee'),
-                        'icon' => 'far fa-id-card',
-                        'url' => '#',
-                        'items' => [
-                            ['label' => Yii::t('app','Текушая комиссия'), 'icon' => 'file-code-o', 'url' => ['/']],
-                            ['label' => Yii::t('app','Архив комиссий'), 'icon' => 'dashboard', 'url' => ['/']],
-                            ['label' => Yii::t('app','Заявления'), 'icon' => 'dashboard', 'url' => ['/']],
-                            ['label' => Yii::t('app','Абитуриенты'), 'icon' => 'dashboard', 'url' => ['/']],
-                        ],
-                    ],
+//                    [
+//                        'label' => Yii::t('app','Selection committee'),
+//                        'icon' => 'far fa-id-card',
+//                        'url' => '#',
+//                        'items' => [
+//                            ['label' => Yii::t('app','Текушая комиссия'), 'icon' => 'file-code-o', 'url' => ['/']],
+//                            ['label' => Yii::t('app','Архив комиссий'), 'icon' => 'dashboard', 'url' => ['/']],
+//                            ['label' => Yii::t('app','Заявления'), 'icon' => 'dashboard', 'url' => ['/']],
+//                            ['label' => Yii::t('app','Абитуриенты'), 'icon' => 'dashboard', 'url' => ['/']],
+//                        ],
+//                    ],
                     [
                         'label' => Yii::t('app','Картотека'),
                         'icon' => 'far fa-list-alt',
@@ -36,14 +36,14 @@
                             ['label' => Yii::t('app','Распределение'), 'icon' => 'dashboard', 'url' => ['/group/allocate']],
                         ],
                     ],
-                    [
-                        'label' => Yii::t('app','Хозяйственная часть'),
-                        'icon' => 'far fa-building',
-                        'url' => '#',
-                        'items' => [
-                            ['label' => Yii::t('app','Здания'), 'icon' => 'file-code-o', 'url' => ['/']],
-                        ],
-                    ],
+//                    [
+//                        'label' => Yii::t('app','Хозяйственная часть'),
+//                        'icon' => 'far fa-building',
+//                        'url' => '#',
+//                        'items' => [
+//                            ['label' => Yii::t('app','Здания'), 'icon' => 'file-code-o', 'url' => ['/']],
+//                        ],
+//                    ],
                     [
                         'label' => Yii::t('app','Настройки'),
                         'icon' => 'cog',
@@ -52,9 +52,9 @@
                             ['label' => Yii::t('app','Специальности'), 'icon' => 'file-code-o', 'url' => ['/speciality']],
                             ['label' => Yii::t('app','Предметы'), 'icon' => 'file-code-o', 'url' => ['/institution-discipline']],
                             ['label' => Yii::t('app','Курсы'), 'icon' => 'file-code-o', 'url' => ['/course/index']],
-                            ['label' => Yii::t('app','Учебный процесс'), 'icon' => 'file-code-o', 'url' => ['/']],
+//                            ['label' => Yii::t('app','Учебный процесс'), 'icon' => 'file-code-o', 'url' => ['/']],
                             ['label' => Yii::t('app','Организация'), 'icon' => 'file-code-o', 'url' => ['/institution']],
-                            ['label' => Yii::t('app','Система рейтинга'), 'icon' => 'file-code-o', 'url' => ['/']],
+//                            ['label' => Yii::t('app','Система рейтинга'), 'icon' => 'file-code-o', 'url' => ['/']],
                         ],
                     ],
                 ],
