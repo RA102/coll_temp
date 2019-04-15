fe6d94068c12364f138b67771746dcf37b75d9fe
Anuar Tastembekov <tastembekov.anuar@gmail.com>
2019-04-15 10:42:48 +0600

    Setup environment production settings

--- environments/prod/common/config/params-local.php
+++ environments/prod/common/config/params-local.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'pds_access_token' => 'change_real_access_token',
-    'pds_url' => 'http://api.pds.loc/',
+    'pds_url' => 'https://pds.cit-orleu.kz/',
     'session_timeout' => 3600 * 24 * 7,
     'bilimal_notifications_access_token' => 'change_real_access_token'
 ];
