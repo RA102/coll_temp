b6533185bfa3a52587e31034a9fa120203d4c4c3
Anuar Tastembekov <tastembekov.anuar@gmail.com>
2019-04-26 05:07:42 +0600

    Remove redundant persons

---
+++ console/migrations/m190425_212025_remove_redundant_persons.php
@@ -0,0 +1,80 @@
+<?php
+
+use common\models\person\Person;
+use common\models\person\PersonCredential;
+use yii\db\Migration;
+
+/**
+ * Class m190425_212025_remove_redundant_persons
+ */
+class m190425_212025_remove_redundant_persons extends Migration
+{
+    private $max_id = 1298702;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        /* @var Person[] $persons */
+        $persons = Person::find()->where(['<=', 'id', $this->max_id])->all();
+        $i = $j = 0;
+        foreach ($persons as $person) {
+            if (count($person->institutions) > 0) {
+                continue;
+            }
+
+            if ($person->portal_uid !== null) {
+
+                /* @var Person[] $otherPersons */
+                $otherPersons = Person::find()
+                    ->where([
+                        'AND',
+                        ['portal_uid' => $person->portal_uid],
+                        ['>', 'id', $this->max_id]
+                    ])
+                    ->all();
+
+                if (count($otherPersons) > 0) {
+                    echo $person->id . ": " . $person->getFullName() . ": " . $person->iin . "\n";
+                    foreach ($otherPersons as $otherPerson) {
+                        echo $otherPerson->id . ": " . $otherPerson->getFullName() . ": " . $otherPerson->iin . "\n";
+                        $this->moveCredentials($person, $otherPerson);
+                        $i++;
+                    }
+                    $person->delete();
+                    echo "--- \n";
+                    $j++;
+                }
+            }
+        }
+
+        echo "Person Found: " . $j . "\n";
+        echo "Other person Found: " . $i . "\n";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+
+    }
+
+    private function moveCredentials(Person $person, Person $newPerson)
+    {
+        $this->update(Person::tableName(), [
+            'firstname' => $person->firstname,
+            'lastname' => $person->lastname,
+            'middlename' => $person->middlename,
+            'iin' => $person->iin
+        ], [
+            'id' => $newPerson->id
+        ]);
+
+        $this->execute('UPDATE ' . PersonCredential::tableName() . ' SET person_id = :new_person_id WHERE person_id = :person_id', [
+            ':person_id' => $person->id,
+            ':new_person_id' => $newPerson->id
+        ]);
+    }
+}
