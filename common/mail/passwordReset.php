23102776fa6c9576b980e43063bbf598fda3bd2c
Anuar Tastembekov <tastembekov.anuar@gmail.com>
2019-04-29 12:00:59 +0600

    debug reset link

--- common/mail/passwordReset.php
+++ common/mail/passwordReset.php
@@ -1,12 +1,14 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $token string */
 
 $host = Yii::$app->params['college_host'];
-$resetLink = $host . Yii::$app->urlManager->createUrl(['site/reset-password', 'token' => $token]);
+$resetLink = Url::toRoute(['site/reset-password', 'token' => $token], $host);
+//$resetLink = $host . Yii::$app->urlManager->createUrl(['site/reset-password', 'token' => $token]);
 ?>
 <div class="password-reset">
     <p>Пройдите по ссылке для изменения пароля:</p>
