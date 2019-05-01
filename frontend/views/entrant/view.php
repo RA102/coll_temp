24fa9937a8b30063a44250ea49d2d454901aed76
Azamat Sarsenbayev <sarsenbayev.a.s@gmail.com>
2019-05-01 12:29:05 +0600

    feat(entrants): view make better look

--- frontend/views/entrant/view.php
+++ frontend/views/entrant/view.php
@@ -1,64 +1,68 @@
 <?php
 
+use common\helpers\PersonHelper;
+use common\models\person\Entrant;
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\person\Entrant */
 
-$this->title = $model->id;
+$this->title = $model->getFullName();
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Entrants'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
-<div class="entrant-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<div style="position: relative;">
+    <h1 class="pull-left"><?=$this->title?></h1>
+</div>
+<div class="clearfix"></div>
 
-    <p>
-        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data'  => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                'method'  => 'post',
-            ],
-        ]) ?>
-    </p>
+<div class="card">
+    <div class="card-body">
+        <div class="entrant-view">
 
-    <?= DetailView::widget([
-        'model'      => $model,
-        'attributes' => [
-            'id',
-            'status',
-            'nickname',
-            'firstname',
-            'lastname',
-            'middlename',
-            'birth_date',
-            'sex',
-            'nationality_id',
-            'iin',
-            'is_pluralist',
-            'birth_country_id',
-            'birth_city_id',
-            'birth_place',
-            'language',
-            'oid',
-            'alledu_id',
-            'alledu_server_id',
-            'pupil_id',
-            'owner_id',
-            'server_id',
-            'is_subscribed:boolean',
-            'portal_uid',
-            'photo',
-            'type',
-            'create_ts',
-            'delete_ts',
-            'import_ts',
-            'person_type',
-        ],
-    ]) ?>
+            <?= DetailView::widget([
+                'model'      => $model,
+                'attributes' => [
+                    'id',
+//                    'status',
+//                    'nickname',
+                    'firstname',
+                    'lastname',
+                    'middlename',
+                    'birth_date',
+                    [
+                        'attribute' => 'sex',
+                        'value' => function(Entrant $model) {
+                            return PersonHelper::getSexList()[$model->sex];
+                        }
+                    ],
+                    [
+                        'attribute' => 'nationality_id',
+                        'value' => function(Entrant $model) {
+                            return $model->nationality->name;
+                        }
+                    ],
+                    'iin',
+//                    'is_pluralist',
+//                    'birth_country_id',
+//                    'birth_city_id',
+                    'birth_place',
+//                    'language',
+//                    'oid',
+//                    'alledu_id',
+//                    'alledu_server_id',
+//                    'pupil_id',
+//                    'owner_id',
+//                    'server_id',
+//                    'is_subscribed:boolean',
+//                    'portal_uid',
+//                    'photo'
+                ],
+            ]) ?>
 
+        </div>
+    </div>
 </div>
