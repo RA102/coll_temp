670eab23034bf6700a7f15545281fac2d8f2cc5f
Azamat Sarsenbayev <sarsenbayev.a.s@gmail.com>
2019-05-10 03:21:30 +0600

    feat(entrance exam order): find applications from current commission

--- frontend/controllers/reception/EntranceExamOrderController.php
+++ frontend/controllers/reception/EntranceExamOrderController.php
@@ -2,20 +2,16 @@
 
 namespace frontend\controllers\reception;
 
+use common\helpers\ApplicationHelper;
+use common\models\reception\AdmissionApplication;
 use common\models\reception\AdmissionProtocol;
-use common\services\person\EntrantService;
 use frontend\models\forms\EntranceExamOrderForm;
 use Yii;
-use common\models\reception\AppealApplication;
-use frontend\search\AppealApplicationSearch;
 use yii\filters\AccessControl;
 use yii\web\Controller;
-use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
-use yii\base\Module;
 
 /**
- * AppealApplicationController implements the CRUD actions for AppealApplication model.
+ * EntranceExamOrderController
  */
 class EntranceExamOrderController extends Controller
 {
@@ -50,7 +46,10 @@ class EntranceExamOrderController extends Controller
         $protocols = AdmissionProtocol::find()->where(['commission_id' => $commission_id])->all();
 
         if ($form->load(Yii::$app->request->post()) && $form->validate()) {
-
+            $successStatuses = [ApplicationHelper::STATUS_ACCEPTED, ApplicationHelper::STATUS_ENLISTED];
+            $applications = AdmissionApplication::find()
+                ->where(['commission_id' => $commission_id, 'status' => $successStatuses])
+                ->all();
         }
 
         return $this->render('index', [
