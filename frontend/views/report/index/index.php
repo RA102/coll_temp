189da79967ac723e48b61e0d39f776acd4ecdd95
Azamat Sarsenbayev <sarsenbayev.a.s@gmail.com>
2019-05-12 12:57:39 +0600

    feat(admission order): add link to report index

--- frontend/views/report/index/index.php
+++ frontend/views/report/index/index.php
@@ -29,11 +29,21 @@ $this->params['breadcrumbs'][] = $this->title;
         </a>
     </div>
     <div class="col-md-4">
-        <a href="<?= \yii\helpers\Url::to(['/reception/entrance-exam-order', 'commission_id' => $commission->id]) ?>">
+        <a href="<?= \yii\helpers\Url::to(['/reception/entrance-exam-order/index', 'commission_id' => $commission->id]) ?>">
             <div class="card">
                 <div class="card-body text-center">
                     <i class="fa fa-file-alt fa-3x"></i>
                     <h4><?= Yii::t('app', 'Order of admission to exams') ?></h4>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="col-md-4">
+        <a href="<?= \yii\helpers\Url::to(['/reception/admission-order/index', 'commission_id' => $commission->id]) ?>">
+            <div class="card">
+                <div class="card-body text-center">
+                    <i class="fa fa-file-alt fa-3x"></i>
+                    <h4><?= Yii::t('app', 'Order of admission') ?></h4>
                 </div>
             </div>
         </a>
