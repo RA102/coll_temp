08046cade308d6c1e9dd658a3babc96eafda014e
Aidyn Yerdibayev <yerdibayev@gmail.com>
2019-10-15 12:38:22 +0600

    order generate

---
+++ frontend/views/order/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$this->title = 'Приказы';
+?>
+
+<h1><?=$this->title?></h1>
+<a href="create-word">word</a>
+
+<div class="card-body skin-white">
+    <h2>Для обучающихся</h2>
+    <ul>
+        <li><a href="students?template=01.docx">Приказ о восстановлении</a></li>
+        <li><a href="students?template=02.docx">Приказ о переводе</a></li>
+        <li><a href="students?template=03.docx">Приказ об отчислении</a></li>
+        <li><a href="students?template=04.docx">Приказ о вручении диплома</a></li>
+        <li><a href="students?template=05.docx">Приказ о допуске к госэкзаменам</a></li>
+        <li><a href="students?template=06.docx">Приказ о назначении стипендии</a></li>
+        <li><a href="students?template=07.docx">Приказ о направлении на практику</a></li>
+        <li><a href="students?template=08.docx">Приказ об условном переводе</a></li>
+        <li><a href="students?template=09.docx">Приказ о дорожных расходах</a></li>
+        <li><a href="students?template=10.docx">Приказ о льготах</a></li>
+        <li><a href="students?template=11.docx">Приказ о питании</a></li>
+        <li><a href="students?template=12.docx">Приказ о переводе госзаказа</a></li>
+        <li><a href="students?template=13.docx">Справка об обучении</a></li>
+        <li><a href="students?template=14.docx">Справка о подтверждении диплома</a></li>
+    </ul>
+</div>
