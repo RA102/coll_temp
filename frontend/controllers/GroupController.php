30317728cb855b8ea05dc2049a2bd6f9677d1892
Aidyn Yerdibayev <yerdibayev@gmail.com>
2019-10-23 18:03:13 +0600

    Update GroupController.php

--- frontend/controllers/GroupController.php
+++ frontend/controllers/GroupController.php
@@ -65,7 +65,7 @@ class GroupController extends Controller
     public function actionIndex()
     {
         $searchModel = new GroupSearch();
-        //$searchModel->institution_id = Yii::$app->user->identity->institution->id;
+        $searchModel->institution_id = Yii::$app->user->identity->institution->id;
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         return $this->render('index', [
