eb60ccb722ed22b78c6b99fd936d7b8ed204ec0c
Artur Rabbe <arturrabbe@gmail.com>
2020-04-03 20:27:55 +0600

    messages fix

--- common/mail/passwordReset.php
+++ common/mail/passwordReset.php
@@ -9,7 +9,10 @@ $host = Yii::$app->params['college_host'];
 $resetLink = $host . Yii::$app->urlManager->createUrl(['site/reset-password', 'token' => $token]);
 ?>
 <div class="password-reset">
-    <p>Пройдите по ссылке для изменения пароля:</p>
+    <p>Вы получили это письмо, потому что запросили смену пароля. Пройдите по временной ссылке для изменения пароля:</p>
 
     <p><?= Html::a(Html::encode($resetLink), $resetLink) ?></p>
+    
+    <p><h5>Обращаем Ваше внимание, что ссылка одноразовая, Вы не сможете ее активировать повторно!</h5></p>
+    <p>Если указанная ссылка не работает, вы можете запросить новую на <a href="https://college.bilimal.kz/site/request-password-reset">форме восстановления.</a></p>
 </div>
