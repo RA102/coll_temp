cf891f35bcbe0c38b569487acfe980cb4f195a42
RA102 <ra_lebedev@mail.com>
2020-06-02 15:54:55 +0600

    сделал фильтр на Предметы

--- frontend/views/course/index.php
+++ frontend/views/course/index.php
@@ -17,6 +17,12 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= $this->title ?></h1>
     <?= Html::a('Добавить', ['create'], ['class' => 'title-action btn btn-primary']) ?>
 </div>
+<!--
+<?php /*echo "<pre>";
+var_dump(ArrayHelper::getColumn(Course::find()->all(), 'classes'));
+echo "</pre>"; */?>
+
+-->
 
 <div class="group-index skin-white">
 
@@ -24,6 +30,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= GridView::widget([
             'dataProvider' => $dataProvider,
             'filterModel' => $searchModel,
+
             'columns' => [
                 ['class' => 'yii\grid\SerialColumn'],
 
@@ -35,13 +42,14 @@ $this->params['breadcrumbs'][] = $this->title;
                 ],*/
                 [
                     'attribute' => 'institution_discipline_id',
+                    'filter' => Html::activeDropDownList($searchModel, 'institution_discipline_id', ArrayHelper::map(Course::find()->all(), 'institution_discipline_id', function($model){ return $model->institutionDiscipline->caption_current; }), ['prompt' => '', 'class' => 'form-control form-control-sm']),
                     'value' => function (Course $model) {
                         return $model->institutionDiscipline->caption_current;
                     },
                 ],
                 [
                     'attribute' => 'classes',
-                    'filter' =>  null, //Html::activeDropDownList($searchModel, 'classes', ArrayHelper::map(Course::find()->all(), 'classes', 'classes'), ['prompt' => '', 'class' => 'form-control form-control-sm']),
+                    'filter' => Html::dropDownList('classes', ArrayHelper::getColumn(Course::find()->all(), 'classes')),
                     'value' => function (Course $model) {
                         return implode(', ', $model->classes);
                     }
