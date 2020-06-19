32206c5aeae1e2fa8958d99c391b171731cd2d42
Artur Rabbe <arturrabbe@gmail.com>
2020-06-19 10:54:02 +0600

    dev fix

--- console/migrations/m200619_043233_create_institution_department_table.php
+++ console/migrations/m200619_043233_create_institution_department_table.php
@@ -5,7 +5,7 @@ use yii\db\Migration;
 /**
  * Handles the creation of table `{{%institution_department}}`.
  */
-class m200618_113201_create_institution_department_table extends Migration
+class m200619_043233_create_institution_department_table extends Migration
 {
     /**
      * {@inheritdoc}
@@ -15,7 +15,7 @@ class m200618_113201_create_institution_department_table extends Migration
         $this->createTable('organization.institution_department', [
             'id' => $this->primaryKey(),
             'institution_id' => $this->integer()->notNull(),
-            'caption' => $this->{},
+            'caption' => 'jsonb',
             'create_ts' => $this->dateTime()->notNull()->defaultValue('now()'),
             'update_ts' => $this->dateTime()->notNull()->defaultValue('now()'),
             'delete_ts' => $this->dateTime()->null(),
