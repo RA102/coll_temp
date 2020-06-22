3e45ad25f6a06d83666cb0f272c653436a60ab2b
Dastan <dastan-4@mail.ru>
2020-06-22 17:52:27 +0600

    C-9805 add dialog for delete and update , add vuetify

--- frontend/views/workload/workloadgroup/index.php
+++ frontend/views/workload/workloadgroup/index.php
@@ -13,21 +13,36 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <?php $this->beginPage() ?>
     <!DOCTYPE html>
+
     <html lang="<?= Yii::$app->language ?>">
     <head>
         <meta charset="<?= Yii::$app->charset ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
     </head>
     <!-- import CSS -->
     <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
+    <!-- Vuetify  -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+    <!-- Vuetify  -->
+
       <!-- import Vue before Element -->
-    <script src="https://unpkg.com/vue/dist/vue.js"></script>
+    <!-- <script src="https://unpkg.com/vue/dist/vue.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
+
     <!-- import JavaScript -->
     <script src="https://unpkg.com/element-ui/lib/index.js"></script>
     <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+
 
     <body class="hold-transition skin-black-light sidebar-mini">
    
@@ -36,7 +51,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
 
 
-<div id="app">
+<v-app id="app">
     <!-- Фильтры -->
     <div class="card-body skin-white">
         <div class="row" style="margin-top: 2px;" >
@@ -164,25 +179,57 @@ $this->params['breadcrumbs'][] = $this->title;
     <div>&nbsp;
     </div>
     <!-- Таблица -->
-    <div class="card-body skin-white">
+    <div class="card-body skin-white" >
         <template>
         <el-table
             :data="tableData"
             style="width: 100%">
             <el-table-column type="expand">
-                <template slot-scope="props">
-                    <p>State: {{ props.row.state }}</p>
-                    <p>City: {{ props.row.city }}</p>
-                    <p>Address: {{ props.row.address }}</p>
-                    <p>Zip: {{ props.row.zip }}</p>
-                </template>
+                <div  slot-scope="props">
+                    <div v-for="(teacher,index) in props.row.teachers" :key="index" >
+                    <div class="row" >
+                        <div class="col-md-2 column-name">
+                            {{teacher.name}}
+                        </div>
+                        <div class="col-md-1 column">
+                            {{teacher.total}}
+                        </div>
+                        <template v-for="(grades,gradeIndex) in teacher.semester" >
+                            <div class="col-md-1 column" :key="gradeIndex">
+                                {{teacher.semester[gradeIndex]}}
+                            </div>
+                            <div class="col-md-1 column">
+                                {{teacher.theory[gradeIndex]}}
+                            </div>
+                            <div class="col-md-1 column">
+                                {{teacher.laboratory[gradeIndex]}}
+                            </div>
+                        </template>
+                        <template class="col-md-2">
+                            <div class="col-md-1 mr-4">
+                            <v-btn 
+                                @click="updateTeacherData(props.row.id,teacher.num)"
+                                type="success" 
+                                 rounded >Изменить</v-btn>
+                            </div>
+                            <div class="col-md-1 ml-4">
+                            <v-btn                                 
+                                type="error" 
+                                @click="openDialogDelete(props.row.id,teacher.num)"
+                                rounded >Удалить</v-btn>
+                            </div>
+                        </template>
+                        
+                    </div>
+                   </div>
+                </div>
             </el-table-column>
             <el-table-column
                 label="Индекс"
                 prop="date">
             </el-table-column>
             <el-table-column
-                label="Дисциплина"
+                label="Найменования дисциплина"
                 prop="name">
             </el-table-column>
             <el-table-column label="Объем учебного времени">
@@ -241,16 +288,110 @@ $this->params['breadcrumbs'][] = $this->title;
         </el-table>
 
         </template>
+        
+
+
+           <!-- Dialog window -->
+    <v-dialog
+      v-model="dialogWindow"
+      max-width="550"
+    >
+      <v-card>
+        <v-card-title class="headline">Изменение данных учителя</v-card-title>
+
+        <v-form
+            ref="form" 
+            v-model="valid"
+            class="pl-6 pr-6 d-flex flex-wrap justify-space-around align-center">
+            <span class="d-flex justify-space-around align-center teacherName">
+                Имя:
+                <v-text-field v-model="name" class="ml-4">
+                </v-text-field>
+            </span>
+            <span v-for="(semesterTemp,index) in semester"  :key="'semester'+index">
+                Семестер номер {{index+1}}
+                <v-text-field
+                    :rules="textFieldRules"
+                    type="number"
+                    v-model="semester[index]"
+                    max="100"
+
+                 ></v-text-field>
+            </span>
+            <span v-for="(item,index) in theory" :key="'theory'+index">
+                Теория {{index+1}} семестр
+                <v-text-field 
+                    :rules="textFieldRules"
+                    v-model="theory[index]" 
+                    type="number"
+                    max="100"
+                    
+                    >
+                </v-text-field>
+            </span>
+            <span v-for="(item,index) in laboratory " :key="'laboratory'+index">
+                Практическая работа {{index+1}} семестр
+                <v-text-field 
+                    :rules="textFieldRules"
+                    v-model="laboratory[index]" 
+                    type="number"
+                    max="100"
+                    >
+                </v-text-field>
+            </span>
+        </v-form>
+
+            <v-card-actions>
+                <v-spacer></v-spacer>
+
+                <v-btn
+                  color="green darken-1"
+                  text
+                  @click="saveDialogData()"
+                >
+                  Сохранить
+                </v-btn>
+
+                <v-btn
+                color="yellow darken-1"
+                text
+                @click="clearDialogWindow()"
+                 >
+                    Отмена
+                </v-btn>
+            </v-card-actions>
+            </v-card>
+        </v-dialog>
+
+        <v-dialog
+            v-model="dialogDeleteTeacher"
+            max-width="400"
+            >
+            <v-card class="dialogDelete">
+                <v-card-title class="headline">Подтвердите удаление</v-card-title>
+                <v-card-actions class="ml-4 mt-2">
+                    <v-btn class="mr-6" color="error" @click="confirmDelete()">Подтвердить</v-btn>
+                    <v-btn color="success" @click="cancelDialogDelete()">Отмена</v-btn>
+                <v-card-actions>
+            </v-card>
+        </v-dialog>
     </div>
-</div>
+ 
+</v-app>
+
 
 <script>
         var startApp = {};
         var init = function () {
             wlApp = new Vue({
                 el: '#app',
-                data: {
+                vuetify:new Vuetify(),
+                data(){
+                    return{
+                    valid:true,
 
+                    dialogDeleteTeacher:false,
+                    dialogWindow:false,
 
                     currentDate: new Date().toTimeString(),
                    
@@ -330,29 +471,68 @@ $this->params['breadcrumbs'][] = $this->title;
                     ],
                     filter_rup: '0',
 
-                    tableData: [{                        
+                    tableData: [{    
+                        id:1,                    
                         date: '2016-05-06',
                         name: 'Tom',
                         state: 'California',
                         city: 'Los Angeles',
                         address: 'No. 189, Grove St, Los Angeles',
-                        zip: 'CA 90036'
-                        }, {
+                        zip: 'CA 90036',
+                        teachers:[
+                            {
+                                num:1,
+                                name:"Омарова С.К.",
+                                total:20,
+                                semester:[10,10],
+                                theory:[10,10],
+                                laboratory:[0,0]
+                            },
+                            {
+                                num:2,
+                                name:"Komarov R.X.",
+                                total:20,
+                                semester:[20,30],
+                                theory:[50,20],
+                                laboratory:[1,5]
+                            }
+                        ]
+            
+                        }, 
+                        {
+                        id:2,
                         date: '2016-05-07',
                         name: 'Tom',
                         state: 'California',
                         city: 'Los Angeles',
                         address: 'No. 189, Grove St, Los Angeles',
-                        zip: 'CA 90036'
-                        }],
+                        zip: 'CA 90036',
+                        teachers:[
+                            {
+                                num:1,
+                                name:"Омарова С.К.",
+                                total:20,
+                                semester:[10,10],
+                                theory:[10,10],
+                                laboratory:[0,0]
+                            }
+                        ]
+                    }],
                     
 
+                    name:"",
+                    semester:[0,0],
+                    theory:[0,0],
+                    laboratory:[0,0],
+                    teacherNum:0,
+                    tableDataId:0,
 
-
-
+                    textFieldRules:[
+                        v=>v<101 && v>=0||"Значение от 0 до 100"
+                    ]
 
                 
-                
+                    }
                 },
 
                 mounted: function () {
@@ -397,6 +577,89 @@ $this->params['breadcrumbs'][] = $this->title;
                         });
 
                     },
+                    updateTeacherData(id,num){
+                        //Изменить учительские данные
+                        this.tableDataId=id;
+                        this.teacherNum=num;
+                        this.updateOrGetTecherData(id,num,0);
+                        //отобразить окно
+                        this.dialogWindow=true;
+                        
+                    },
+                    clearDialogWindow(){
+                        //Закрваем диалоговое окно с кнопки отмена
+                        this.dialogWindow=false;
+                        this.name="";
+                        this.semester=[0,0];
+                        this.theory=[0,0];
+                        this.laboratory=[0,0];
+                        this.tableDataId=0;
+                        this.teacherNum=0;
+                    },
+                    updateOrGetTecherData(id,num,type){
+                        //type 0=получить 1=сохранить 2=удалить
+                        for(let i=0;i<this.tableData.length;i++){
+                            if(this.tableData[i].id==id){
+                               
+                                for(let j=0;j<this.tableData[i].teachers.length;j++){
+                                    if(this.tableData[i].teachers[j].num==num){
+                                        if(type==0){                                         
+                                            
+                                            this.name=this.tableData[i].teachers[j].name;
+
+                                            this.semester[0]=this.tableData[i].teachers[j].semester[0];
+                                            this.semester[1]=this.tableData[i].teachers[j].semester[1];
+                                            
+                                            this.theory[0]=this.tableData[i].teachers[j].theory[0];
+                                            this.theory[1]=this.tableData[i].teachers[j].theory[1];
+
+                                            this.laboratory[0]=this.tableData[i].teachers[j].laboratory[0];
+                                            this.laboratory[1]=this.tableData[i].teachers[j].laboratory[1];
+
+
+                                        }else if(type==1){
+
+                                            this.tableData[i].teachers[j].name=this.name;
+                                            this.tableData[i].teachers[j].semester=this.semester;
+                                            this.tableData[i].teachers[j].theory=this.theory;
+                                            this.tableData[i].teachers[j].laboratory=this.laboratory;
+
+                                        }else if(type==2){
+                                            
+                                            this.tableData[i].teachers.splice(j,1);
+                                           
+
+                                        }break;
+                                    }
+                                }break;                                
+                            }
+                        }
+
+                    },
+                    saveDialogData(){
+                        //сохраняем данные
+                        if(this.$refs.form.validate()){
+                            this.updateOrGetTecherData(this.tableDataId,this.teacherNum,1);
+                            this.clearDialogWindow();
+                        }
+                    },
+                    confirmDelete(){
+                        this.dialogDeleteTeacher=false;
+                        this.updateOrGetTecherData(this.tableDataId,this.teacherNum,2)
+                    },
+                    openDialogDelete(id,num){
+                        this.tableDataId=id;
+                        this.teacherNum=num;                        
+                        this.dialogDeleteTeacher=true;
+                    },
+                    cancelDialogDelete(){
+                        this.dialogDeleteTeacher=false;
+                        this.tableDataId=0;
+                        this.teacherNum=0;
+                    }
+
+
+
 
                     
 
@@ -437,6 +700,24 @@ $this->params['breadcrumbs'][] = $this->title;
     </script>
 
     <style>
+    .dialogDelete{
+        height:150px;
+    }
+    .teacherName{
+        width:400px;
+    }
+    .column-name{
+        display:flex;
+        justify-content:center;
+    }
+    .column {
+        padding-right:10px;
+        display:flex;
+        justify-content:flex-start;
+    }
+    .nameDisciplines{
+        width:300px;
+    }
     .demo-input-label {
         display: inline-block;
         width: 40px;
